f466faa9fef0868ed71fe6ccb20acfc93ff39513
Xiangshi Yin <piupiuyin@gmail.com>
2023-09-24 14:33:17 -0400

    notebook template included

--- utils/generate_folders.py
+++ utils/generate_folders.py
@@ -29,6 +29,50 @@ Here are the topics we are going to cover
   * TBD
 """
 
+notebook_template = """
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "(**You can also open this notebook in Google Colab**)\\n",
+    "\\n",
+    "[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/xiangshiyin/data-programming-with-python/blob/main/@semester@/@week@/notebook/code_demo.ipynb)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "data-programming-with-python-h2fMFruk-py3.10",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.10.8"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
+"""
+
 def create_folder_if_not_exist(folder_path):
     if os.path.exists(folder_path) and os.path.isdir(folder_path):
         print(f"Folder {folder_path} already exists.")
@@ -59,17 +103,28 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="This module is used to generate folders and files for every weekly class")
     parser.add_argument("--semester", type=str, required=True, help="Pick the semester folder")
     parser.add_argument("--week", type=str, help="Define the weekly folder name")
+    parser.add_argument("--notebook", action="store_true", help="generate jupyter notebook")
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Check and create the weekly folder
+    week_dir_path = f"./{args.semester.strip('/')}/{args.week.strip('/')}"
+    notebook_dir_path = f"{week_dir_path}/notebook"
+    readme_file_path = f"{week_dir_path}/README.md"
+    notebook_file_path = f"{notebook_dir_path}/code_demo.ipynb"
+
     create_folder_if_not_exist(args.semester)
-    create_folder_if_not_exist(f"./{args.semester.strip('/')}/{args.week.strip('/')}")
-    create_folder_if_not_exist(f"./{args.semester.strip('/')}/{args.week.strip('/')}/notebook")
-    create_file_if_not_exist(
-        file_path=f"./{args.semester.strip('/')}/{args.week.strip('/')}/README.md",
-        template=readme_template
-    )
+    create_folder_if_not_exist(week_dir_path)
+    create_folder_if_not_exist(notebook_dir_path)
+    if args.notebook:
+        create_file_if_not_exist(
+            file_path=notebook_file_path,
+            template=notebook_template.replace(
+                '@semester@', args.semester
+            ).replace(
+                '@week@', args.week
+            )
+        )
 
 
